57e79fe02265c9a859d959b929663dc0b39977ba
maneeshpal <maneeshpal539@gmail.com>
2015-05-07 09:02:16 -0500

    just lie that

--- static/js/src/todo.js
+++ static/js/src/todo.js
@@ -5,24 +5,51 @@ var isNode = typeof module !== 'undefined'
   && typeof window === 'undefined';
 var React = isNode ? require('react/addons') : window.React;
 
+
+var TodoItem = React.createClass({
+  render: function() {
+    
+    return (
+      <div>
+        todo item: {this.props.todo}
+      </div>
+    );
+  }
+});
+
+var TodoList = React.createClass({
+  render: function() {
+    var todoNodes = this.props.data.map(function (comment) {
+      return (
+        <TodoItem todo={comment}>
+        </TodoItem>
+      );
+    });
+    return (
+      <div className="commentList">
+        {todoNodes}
+      </div>
+    );
+  }
+});
+
 var TodoApp = React.createClass({
     componentDidMount: function () {
       console.log('component mounted');
     },
     render: function () {
       return (
-        <div>
-          Maneesh
-        </div>
+        <h4>Todo App heading</h4>
+        <TodoList data = {this.props.data} />
       )
     }
   });
 
 if(isNode) {
-  module.exports.todoApp = TodoApp;  
+  module.exports.todoApp = TodoApp;
 }
 else {
   var mountNode = document.getElementById("react-main-mount");
-
-  React.render(<TodoApp/>, mountNode); 
+  var data = ['item1', 'item2'];
+  React.render(<TodoApp data={data}/>, mountNode); 
 }
